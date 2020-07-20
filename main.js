a484a061eca985e23f6bac8c6ae554f3fac9c0d2
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-21 00:50:26 +0300

    First

---
+++ main.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = ';'; 
+
+client.once('ready', () => {
+console.log('VBot Is online.')
+});
+
+
+client.on('ready', () => {
+    client.user.setActivity('Veernezus Scripting', { type : "WATCHING"})
+    })
+
+client.on('message' ,message => {
+if(!message.content.startsWith(prefix)|| message.author.bot ) return;
+
+const args = message.content.slice(prefix.length).split(/ +/);
+const command = args.shift().toLowerCase();
+
+if (command === 'ping'){
+    message.channel.send('pong');
+}
+
+
+});
+
+
+
+
+client.login(process.env.token);
