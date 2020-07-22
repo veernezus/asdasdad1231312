f71782856c98d3dd275d5807cf649e7038e5ba95
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-22 16:25:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -22,8 +22,7 @@ client.on('ready', () => {
     client.on('guildMemberAdd', member =>{
 const channel = member.guild.channels.cache.find(channel => channel.name === "👋welcome👋");
 if(!channel) return;
-channel.send(`${member}
-\`\`\`\Welcome to BeST AuToFArM ;} !\`\`\``);
+channel.send(`Welcome ${member} To BeST AuToFArM ;} !`);
 
 });
 
