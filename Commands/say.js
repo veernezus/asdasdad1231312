8c67320e3559312c8c79f4aa9221267faed10678
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-23 16:04:18 +0300

    Create say.js

---
+++ Commands/say.js
@@ -0,0 +1,14 @@
+module.exports.run = async (bot,message,args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("no");
+    let botmessage = args.join(" ");
+
+message.delete().catch();
+message.channel.send(botmessage)
+
+
+
+}
+
+module.exports.help = {
+    name: say
+}
