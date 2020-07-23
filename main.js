80d1977295d8356812c9e52c37ad2c5c5a991642
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-23 16:42:28 +0300

    Update main.js

--- main.js
+++ main.js
@@ -36,7 +36,11 @@ if (command === 'ping'){
     message.channel.send('pong');
 }
 else if (command == 'say') {
-client.commands.get('say').execute(message,args);
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("no");
+let botmessage = args.join(" ");
+
+message.delete().catch();
+message.channel.send(botmessage)
 }
 
 });
