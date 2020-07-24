cbb78aebbe8050bb1e6961360d2f3f8ce453b28f
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-25 01:36:04 +0300

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ if (command === 'ping'){
 }
 
 else if (command == 'say') {
-if (!message.member.roles.cache.find(r => r.name === "Staff")) return message.reply('You Do Not Have Permissions, Rip').then((message) => {setTimeout(function(){message.delete()},5000) }) 
+if (!message.member.roles.cache.find(r => r.name === "Staff")) return message.reply('You Do Not Have Permissions, Rip').then((message) => {setTimeout(function(){message.delete()},5000) }); 
 let botmessage = args.join(" ");
 
 message.delete();
