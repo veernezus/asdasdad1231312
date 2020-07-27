5650e1638e312c275f2c3f639a965b2ad6d7bc24
veernezus <67125276+veernezus@users.noreply.github.com>
2020-07-28 00:16:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -33,26 +33,26 @@ member.guild.channels.cache.get('718849959574896801').send(`${member}`);
 
 client.on('message' ,message => {
 if(!message.content.startsWith(prefix)|| message.author.bot ) return;
-message.delete();
+
 if(!message.member.roles.cache.find(r => r.name === "VBot Perms")) return message.reply('You Do Not Have Permissions, Rip').then((message) => {setTimeout(function(){message.delete()},5000) }); 
 
 const args = message.content.slice(prefix.length).split(/ +/);
 const command = args.shift().toLowerCase();
 
-if (command === 'say') {
+if (command === 'ping'){
+    message.channel.send('pong');
+}
 
+else if (command == 'say') {
+    
 let botmessage = args.join(" ");
-
+message.delete();
 message.channel.send(botmessage); 
 };
 
 });
 
-client.on('message' ,message => {
-if(!message.content.startsWith(prefix)|| message.author.bot ) return
-if (command === 'ping'){
-    message.channel.send('pong');
-}
-});
+
+
 
 client.login(process.env.token);
